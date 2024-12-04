a46763a20cf5856779092a263cdc8857d6a06504
Bupe <ngasebupe@gmail.com>
2024-12-04 16:32:00 +0200

    updated error message

--- src/App/Transfers/Dashboard/ErrorMessage.tsx
+++ src/App/Transfers/Dashboard/ErrorMessage.tsx
@@ -7,12 +7,18 @@ interface ErrorMessageProps {
 
 const ErrorMessage: FC<ErrorMessageProps> = () => {
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-      <MessageBox kind="default" style={{ width: '300px', padding: '20px', textAlign: 'center' }}>
-        <h5 style={{ fontSize: '16px', marginBottom: '10px' }}>Restricted Access!</h5>
-        <p style={{ fontSize: '14px' }}>Your access to this resource has been restricted.</p>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'left', marginTop: '-8px',marginBottom: '-8px', marginLeft: '-16px', marginRight: '-16px' }}>
+      <MessageBox kind="default" style={{ width: '100%', textAlign: 'left', marginTop: '-8px',marginBottom: '-8px', marginLeft: '-16px', marginRight: '-16px' }}>
+        <p style={{ fontSize: '14px' }}>Restricted Access</p>
       </MessageBox>
     </div>
+
+    // <div style={{ display: 'flex', justifyContent: 'fill', alignItems: 'flex-start', margin: '0', padding: '0' }}>
+    // <MessageBox kind="default" style={{ width: '100%', textAlign: 'left', margin: '0', padding: '0' }}>
+    //   {/* <h5 style={{ fontSize: '16px', marginBottom: '10px' }}>Restricted Access</h5> */}
+    //   <p style={{ fontSize: '14px', margin: '0' }}>Restricted Access</p>
+    // </MessageBox>
+    // </div>
   );
 };
 
